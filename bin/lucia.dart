c86b026b2c25444bfa9a85fe78db344314afd5ce
Lucia <luciasj400@gmail.com>
2024-04-17 15:24:27 +0200

    primer comic

---
+++ bin/lucia.dart
@@ -0,0 +1,5 @@
+import 'package:lucia/lucia.dart' as lucia;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lucia.calculate()}!');
+}
---
+++ test/lucia_test.dart
@@ -0,0 +1,8 @@
+import 'package:lucia/lucia.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
